00820e696f689b9d49e1a8aed4742744686b2733
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-07-25 08:40:24 +0100

    Update iam_user.tf

--- AWS/IAM/iam_user.tf
+++ AWS/IAM/iam_user.tf
@@ -9,15 +9,86 @@ resource "aws_iam_user" "jenkins" {
   name = var.iam_user_name
 }
 
-resource "aws_iam_user_policy_attachment" "eks_policy_attachment" {
-  user       = aws_iam_user.jenkins.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+resource "aws_iam_policy" "jenkins_user_policy" {
+  name        = "JenkinsUserPolicy"
+  description = "IAM policy for Jenkins user to manage ECR, ECS, and EKS"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ecr:CreateRepository",
+          "ecr:DescribeRepositories",
+          "ecr:GetAuthorizationToken",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetRepositoryPolicy",
+          "ecr:ListImages",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:SetRepositoryPolicy",
+          "ecr:DeleteRepository",
+          "ecr:DeleteRepositoryPolicy"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ecs:RegisterTaskDefinition",
+          "ecs:DeregisterTaskDefinition",
+          "ecs:ListTaskDefinitions",
+          "ecs:DescribeTaskDefinition",
+          "ecs:CreateCluster",
+          "ecs:DeleteCluster",
+          "ecs:DescribeClusters",
+          "ecs:ListClusters",
+          "ecs:ListContainerInstances",
+          "ecs:DescribeContainerInstances",
+          "ecs:ListServices",
+          "ecs:DescribeServices",
+          "ecs:CreateService",
+          "ecs:UpdateService",
+          "ecs:DeleteService",
+          "ecs:RunTask"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "eks:CreateCluster",
+          "eks:DeleteCluster",
+          "eks:DescribeCluster",
+          "eks:ListClusters",
+          "eks:DescribeFargateProfile",
+          "eks:CreateFargateProfile",
+          "eks:DeleteFargateProfile",
+          "eks:UpdateClusterVersion",
+          "eks:UpdateNodegroupVersion",
+          "eks:DescribeAddon",
+          "eks:DescribeAddonVersions",
+          "eks:ListAddons",
+          "eks:CreateAddon",
+          "eks:DeleteAddon",
+          "eks:TagResource",
+          "eks:UntagResource"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
-resource "aws_iam_user_policy_attachment" "ecs_policy_attachment" {
-  user       = aws_iam_user.jenkins.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+resource "aws_iam_user_policy_attachment" "jenkins_user_policy_attachment" {
+  user       = aws_iam_user.jenkins_user.name
+  policy_arn = aws_iam_policy.jenkins_user_policy.arn
 }
+
 
 resource "aws_iam_access_key" "iam_user_access_key" {
   user = aws_iam_user.jenkins.name
