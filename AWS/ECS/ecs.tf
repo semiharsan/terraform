1fc96a2f42607e202caa5a3308db9722601ee381
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-07-25 09:30:52 +0100

    Update ecs.tf

---
+++ AWS/ECS/ecs.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_ecr_repository" "arsit_ecr_repo" {
+  name = var.ecr_repo_name
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
