f73fa089db3586bf43d9cf7afb7316613df0d7d5
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-07-26 23:20:04 +0100

    Update ecr.tf

--- AWS/ECR/ecr.tf
+++ AWS/ECR/ecr.tf
@@ -10,6 +10,10 @@ resource "aws_ecr_repository" "arsit_ecr_repo" {
   }
 }
 
+output "ecr_id" {
+  value = aws_ecr_repository.arsit_ecr_repo.id
+}
+
 output "ecr_registry_id" {
   value = aws_ecr_repository.arsit_ecr_repo.registry_id
 }
