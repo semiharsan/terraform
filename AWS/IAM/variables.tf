93c4aa7d4207756d4621c7cfa0dd6c2a901b98e8
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-07-26 21:37:26 +0100

    Update variables.tf

--- AWS/IAM/variables.tf
+++ AWS/IAM/variables.tf
@@ -1,12 +1,3 @@
-variable "cli_usr_access_key" {
-  type    = string
-  default = "write here your aws administrator user access key"
-}
-variable "cli_usr_secret_key" {
-  type    = string
-  default = "write here your aws administrator user secret key"
-}
-
 variable "aws_region" {
   description = "The AWS region where the resources will be provisioned."
   default     = "eu-west-2"
