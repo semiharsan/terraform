f9161d6e183ae351bd050eeed1ca725dbe8afaba
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-07-28 20:39:55 +0100

    Update ecs.tf

--- AWS/ECS/ecs.tf
+++ AWS/ECS/ecs.tf
@@ -100,6 +100,27 @@ resource "aws_security_group" "load_balancer_security_group" {
 
 
 ################################################################
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_full_access_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+  role       = aws_iam_role.ecs_task_execution_role.name
+}
 
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = var.cluster_name
@@ -127,7 +148,7 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
   network_mode             = "awsvpc"
   memory                   = "512"
   cpu                      = "256"
-  execution_role_arn       = "arn:aws:iam::aws:policy/service-role/AmazonECS_FullAccess"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 }
 
 resource "aws_ecs_service" "ecs_service" {
