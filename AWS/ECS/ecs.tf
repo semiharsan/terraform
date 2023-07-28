d62fdf3aed627169d2ef02e07bdec82f71fb63fe
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-07-28 20:26:25 +0100

    Update ecs.tf

--- AWS/ECS/ecs.tf
+++ AWS/ECS/ecs.tf
@@ -100,79 +100,6 @@ resource "aws_security_group" "load_balancer_security_group" {
 
 
 ################################################################
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecs-task-execution-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "ecs_task_execution_policy" {
-  name = "ecs-task-execution-policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:GetRepositoryPolicy",
-          "ecr:DescribeRepositories",
-          "ecr:ListImages",
-          "ecr:DescribeImages",
-          "ecr:BatchGetImage",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-      {
-        Action = [
-          "ecs:RunTask",
-          "ecs:StartTask",
-          "ecs:StopTask",
-          "ecs:ListTasks",
-          "ecs:DescribeTasks",
-          "ecs:DescribeTaskDefinition",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-      {
-        Action = [
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-        ]
-        Effect   = "Allow"
-        Resource = "arn:aws:logs:*:*:*"
-      },
-      {
-        Action = [
-          "iam:PassRole",
-        ]
-        Effect   = "Allow"
-        Resource = "arn:aws:iam::*:role/*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_attachment" {
-  policy_arn = aws_iam_policy.ecs_task_execution_policy.arn
-  role       = aws_iam_role.ecs_task_execution_role.name
-}
-
 
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = var.cluster_name
@@ -200,7 +127,7 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
   network_mode             = "awsvpc"
   memory                   = "512"
   cpu                      = "256"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = "arn:aws:iam::aws:policy/service-role/AmazonECS_FullAccess"
 }
 
 resource "aws_ecs_service" "ecs_service" {
