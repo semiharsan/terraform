63d85b99f2279a181a3637a0d40b96c0612888e8
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-08-24 14:55:55 +0100

    Update variables.tf

--- AWS/EKS/variables.tf
+++ AWS/EKS/variables.tf
@@ -1,17 +1,21 @@
 variable "region" {
+  default     =var.REGION
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
+  default     = var.VPC_CIDR
 }
 
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
+  default     = var.CLUSTER_NAME
 }
 
 variable "fargate_profile_name" {
   description = "Name of the Fargate Profile in EKS cluster"
   type        = string
+  default     = var.FARGATE_PROFILE_NAME
 }
