138e75152a146955374bcac4cd9eb67e8c941447
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-08-24 13:31:40 +0100

    Update variables.tf

--- AWS/EKS/variables.tf
+++ AWS/EKS/variables.tf
@@ -1,179 +1,21 @@
-variable "cluster_version" {
-  description = "Kubernetes `<major>.<minor>` version to use for the EKS cluster (i.e.: `1.27`)"
+variable "region" {
+  default     = "eu-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
   type        = string
-  default     = null
+  default     = "10.0.0.0/16"
 }
 
-variable "create" {
-  description = "Controls if EKS resources should be created (affects nearly all resources)"
-  type        = bool
-  default     = true
-}
-
-variable "tags" {
-  description = "A map of tags to add to all resources"
-  type        = map(string)
-  default     = {}
-}
-
-variable "prefix_separator" {
-  description = "The separator to use between the prefix and the generated timestamp for resource names"
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
   type        = string
-  default     = "-"
+  default     = "Arsit-Eks"
 }
 
-variable "openid_connect_audiences" {
-  description = "List of OpenID Connect audience client IDs to add to the IRSA provider"
-  type        = list(string)
-  default     = []
-}
-
-variable "custom_oidc_thumbprints" {
-  description = "Additional list of server certificate thumbprints for the OpenID Connect (OIDC) identity provider's server certificate(s)"
-  type        = list(string)
-  default     = []
-}
-
-variable "cluster_addons" {
-  description = "Map of cluster addon configurations to enable for the cluster. Addon name can be the map keys or set with `name`"
-  type        = any
-  default     = {}
-}
-
-variable "cluster_addons_timeouts" {
-  description = "Create, update, and delete timeout configurations for the cluster addons"
-  type        = map(string)
-  default     = {}
-}
-
-variable "cluster_identity_providers" {
-  description = "Map of cluster identity provider configurations to enable for the cluster. Note - this is different/separate from IRSA"
-  type        = any
-  default     = {}
-}
-
-variable "manage_aws_auth_configmap" {
-  description = "Determines whether to manage the aws-auth configmap"
-  type        = bool
-  default     = false
-}
-
-variable "create_aws_auth_configmap" {
-  description = "Determines whether to create the aws-auth configmap. NOTE - this is only intended for scenarios where the configmap does not exist (i.e. - when using only self-managed node groups). Most users should use `manage_aws_auth_configmap`"
-  type        = bool
-  default     = false
-}
-
-variable "aws_auth_node_iam_role_arns_non_windows" {
-  description = "List of non-Windows based node IAM role ARNs to add to the aws-auth configmap"
-  type        = list(string)
-  default     = []
-}
-
-variable "aws_auth_node_iam_role_arns_windows" {
-  description = "List of Windows based node IAM role ARNs to add to the aws-auth configmap"
-  type        = list(string)
-  default     = []
-}
-
-variable "aws_auth_fargate_profile_pod_execution_role_arns" {
-  description = "List of Fargate profile pod execution role ARNs to add to the aws-auth configmap"
-  type        = list(string)
-  default     = []
-}
-
-variable "aws_auth_roles" {
-  description = "List of role maps to add to the aws-auth configmap"
-  type        = list(any)
-  default     = []
-}
-
-variable "aws_auth_users" {
-  description = "List of user maps to add to the aws-auth configmap"
-  type        = list(any)
-  default     = []
-}
-
-variable "aws_auth_accounts" {
-  description = "List of account maps to add to the aws-auth configmap"
-  type        = list(any)
-  default     = []
-}
-
-################################################################################
-# Cluster IAM Role
-################################################################################
-
-variable "create_iam_role" {
-  description = "Determines whether a an IAM role is created or to use an existing IAM role"
-  type        = bool
-  default     = true
-}
-
-variable "iam_role_arn" {
-  description = "Existing IAM role ARN for the cluster. Required if `create_iam_role` is set to `false`"
+variable "fargate_profile_name" {
+  description = "Name of the Fargate Profile in EKS cluster"
   type        = string
-  default     = null
-}
-
-variable "iam_role_name" {
-  description = "Name to use on IAM role created"
-  type        = string
-  default     = null
-}
-
-variable "iam_role_use_name_prefix" {
-  description = "Determines whether the IAM role name (`iam_role_name`) is used as a prefix"
-  type        = bool
-  default     = true
-}
-
-variable "iam_role_path" {
-  description = "Cluster IAM role path"
-  type        = string
-  default     = null
-}
-
-variable "iam_role_description" {
-  description = "Description of the role"
-  type        = string
-  default     = null
-}
-
-variable "iam_role_permissions_boundary" {
-  description = "ARN of the policy that is used to set the permissions boundary for the IAM role"
-  type        = string
-  default     = null
-}
-
-variable "iam_role_additional_policies" {
-  description = "Additional policies to be added to the IAM role"
-  type        = map(string)
-  default     = {}
-}
-
-# TODO - hopefully this can be removed once the AWS endpoint is named properly in China
-# https://github.com/terraform-aws-modules/terraform-aws-eks/issues/1904
-variable "cluster_iam_role_dns_suffix" {
-  description = "Base DNS domain name for the current partition (e.g., amazonaws.com in AWS Commercial, amazonaws.com.cn in AWS China)"
-  type        = string
-  default     = null
-}
-
-variable "iam_role_tags" {
-  description = "A map of additional tags to add to the IAM role created"
-  type        = map(string)
-  default     = {}
-}
-
-variable "cluster_ip_family" {
-  description = "The IP family used to assign Kubernetes pod and service addresses. Valid values are `ipv4` (default) and `ipv6`. You can only specify an IP family when you create a cluster, changing this value will force a new cluster to be created"
-  type        = string
-  default     = null
-}
-
-variable "iam_role_attach_cni_policy" {
-  description = "Whether to attach the `AmazonEKS_CNI_Policy`/`AmazonEKS_CNI_IPv6_Policy` IAM policy to the IAM IAM role. WARNING: If set `false` the permissions must be assigned to the `aws-node` DaemonSet pods via another method or nodes will not be able to join the cluster"
-  type        = bool
-  default     = true
+  default     = "Arsit-Eks-Fargate"
 }
