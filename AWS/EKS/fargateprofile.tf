fb77a906bb32c02c8d72c2cc2d1caabcbaed5151
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-08-25 14:55:52 +0100

    Update fargateprofile.tf

--- AWS/EKS/fargateprofile.tf
+++ AWS/EKS/fargateprofile.tf
@@ -67,18 +67,8 @@ resource "aws_eks_fargate_profile" "fargate_profile" {
 }
 
 ################Patch CoreDNS Deployment##############################################
-#provider "kubernetes" {
-#  config_path = var.config_path  # Path to your kubeconfig file
-#}
-
 provider "kubernetes" {
-  host                   = aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks_cluster.name]
-    command     = "aws"
-  }
+  config_path = var.config_path   # Path to your kubeconfig file
 }
 
 resource "null_resource" "patch_coredns" {
