9fbd72233a239131cd58bc4056e35410f4813d81
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-08-25 14:19:56 +0100

    Update variables.tf

--- AWS/EKS/variables.tf
+++ AWS/EKS/variables.tf
@@ -1,27 +1,22 @@
 variable "region" {
-  default     = null
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = null
 }
 
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = null
 }
 
 variable "fargate_profile_name" {
   description = "Name of the Fargate Profile in EKS cluster"
   type        = string
-  default     = null
 }
 
 variable "config_path" {
   description = "Path for your Kube config file"
   type        = string
-  default     = null
 }
