3344ea9229f23556357434ddb0959ee1f1429188
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-08-26 13:06:36 +0100

    Update alb_controller.tf

--- AWS/EKS/alb_controller.tf
+++ AWS/EKS/alb_controller.tf
@@ -4,25 +4,6 @@ provider "helm" {
   }
 }
 
-resource "kubernetes_service_account" "aws_load_balancer_controller" {
-  metadata {
-    name      = "aws-load-balancer-controller"
-    namespace = "kube-system"
-
-    labels = {
-      "app.kubernetes.io/component"       = "controller"
-      "app.kubernetes.io/name"            = "aws-load-balancer-controller"
-      "eks.amazonaws.com/fargate-profile" = var.fargate_profile_name
-    }
-
-    annotations = {
-      "eks.amazonaws.com/role-arn" = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/AmazonEKSLoadBalancerControllerRole"
-    }
-  }
-
-  depends_on = [null_resource.patch_coredns]
-}
-
 resource "helm_release" "aws-load-balancer-controller" {
   name = "aws-load-balancer-controller"
 
