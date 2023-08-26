450abdc8df211eb45987755be241636504d8a5b2
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-08-26 14:15:52 +0100

    Update fargateprofile.tf

--- AWS/EKS/fargateprofile.tf
+++ AWS/EKS/fargateprofile.tf
@@ -71,6 +71,14 @@ resource "aws_eks_fargate_profile" "fargate_profile" {
 #  config_path = var.config_path   # Path to your kubeconfig file
 #}
 
+data "aws_eks_cluster" "eks_cluster" {
+  name = aws_eks_cluster.eks_cluster.name
+}
+
+data "aws_eks_cluster_auth" "eks_cluster" {
+  name = aws_eks_cluster.eks_cluster.name
+}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
