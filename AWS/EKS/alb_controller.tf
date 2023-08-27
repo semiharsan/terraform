9660c9c4a75882e9815d9a9af6544eefb38eab8e
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-08-27 13:02:00 +0100

    Update alb_controller.tf

--- AWS/EKS/alb_controller.tf
+++ AWS/EKS/alb_controller.tf
@@ -1,3 +1,11 @@
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.eks_cluster.token
+  }
+}
+
 resource "kubernetes_service_account" "aws_load_balancer_controller" {
   metadata {
     name      = "aws-load-balancer-controller"
