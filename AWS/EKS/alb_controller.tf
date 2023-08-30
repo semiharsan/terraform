e19c223406f68c64c9ff67a39f9f816b2e4eb80f
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-08-30 13:19:32 +0100

    Update alb_controller.tf

--- AWS/EKS/alb_controller.tf
+++ AWS/EKS/alb_controller.tf
@@ -1,21 +1,3 @@
-#provider "helm" {
-#  kubernetes {
-#    config_path = var.config_path  # Path to your kubeconfig file
-#  }
-#}
-
-#provider "helm" {
-#  kubernetes {
-#    host                   = aws_eks_cluster.eks_cluster.endpoint
-#  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority.0.data)
-#  exec {
-#    api_version = "client.authentication.k8s.io/v1beta1"
-#    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-#    command     = "aws"
-#  }
-#  }
-#}
-
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.eks_cluster.endpoint
